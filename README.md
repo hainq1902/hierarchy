78fc29b4089900cfc3040ef99fb9c4ed829cc74c
Hai N <hainq1902@gmail.com>
2023-11-17 10:42:52 -0600

    Add document and more test cases

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # hierarchy
+
+## Requirement
+* Maven
+* JDK 1.8
+
+## Run unit test
+* To run unit test, please run the following command in project folder
+  * `mvn test`
+* It's also possible to run unit test in your favorite IDE by running it through `com.almworks.structure.cloud.commons.util.FilterTest`
+
+## Filter class
+* Class `com.almworks.structure.cloud.commons.util.Filter`
+* Step to travel through the hierarchy and filter
+  1. Start with node index 0
+  2. Find sibling of current node and store in sibling stack
+  3. Test current node against predicate
+  4. If test passes, move to node with next index
+  5. If test fails, move to next sibling in sibling stack
+  6. Go back to step #2 until travel to the end of hierarchy
--- src/test/java/com/almworks/structure/cloud/commons/util/FilterTest.java
+++ src/test/java/com/almworks/structure/cloud/commons/util/FilterTest.java
@@ -21,6 +21,21 @@ public class FilterTest {
     }
 
     @Test
+    public void testFilterManySiblingAndMultiRoot() {
+        Hierarchy unfiltered = new ArrayBasedHierarchy(
+                new int[] {1, 2, 3, 4,  9,  33,  7,  6, 5, 6, 7, 8, 9, 10, 11},
+                new int[] {0, 1, 2, 3,  3,  3,  3,  3, 1, 0, 1, 0, 1, 1, 2}
+        );
+        Hierarchy filteredActual = Filter.filter(unfiltered, nodeId -> nodeId % 3 != 0);
+        Hierarchy filteredExpected = new ArrayBasedHierarchy(
+                new int[] {1, 2, 5, 8, 10, 11},
+                new int[] {0, 1, 1, 0, 1, 2}
+        );
+
+        Assert.assertEquals(filteredExpected.formatString(), filteredActual.formatString());
+    }
+
+    @Test
     public void testFilterWhenOnlyLeftBranch() {
         Hierarchy unfiltered = new ArrayBasedHierarchy(
                 new int[] {1, 2, 3, 4, 5, 5, 7},
@@ -38,8 +53,8 @@ public class FilterTest {
     @Test
     public void testFilterWhenOnlyLeftBranchMultiRoot() {
         Hierarchy unfiltered = new ArrayBasedHierarchy(
-                new int[] {1, 2, 3, 4, 5, 5, 7, 1, 2, 3, 4, 5, 5, 7},
-                new int[] {0, 1, 2, 3, 4, 5, 6, 0, 1, 2, 3, 4, 5, 6}
+                new int[] {1, 2, 3, 4, 9, 33, 7, 5, 5, 7, 1, 2, 3, 4, 5, 5, 7},
+                new int[] {0, 1, 2, 3, 3, 3, 3, 4, 5, 6, 0, 1, 2, 3, 4, 5, 6}
         );
         Hierarchy filteredActual = Filter.filter(unfiltered, nodeId -> nodeId % 3 != 0);
         Hierarchy filteredExpected = new ArrayBasedHierarchy(
